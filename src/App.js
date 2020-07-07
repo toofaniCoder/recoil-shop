9faeebffa546be30e2caa2406e109bb54619038a
toofaniCoder <smartcoder500@gmail.com>
2020-07-07 18:40:26 +0530

    starter commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">hello reactjs</h1>
+    </div>
+  );
+}
+
+export default App;
